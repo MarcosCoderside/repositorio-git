ea94fc213a7db2642611f9ba563fb9d078d70dcc
MarcosCoderside <marcos_ushimata@yahoo.com.br>
2020-01-09 14:31:40 -0300

    second branch

--- app/Views/index.php
+++ app/Views/index.php
@@ -8,16 +8,130 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+  <style rel="stylesheet">
+      .hover-links:hover{
+        color:#000 !important;
+    }
+  </style>
+  
   </head>
   <body>
       
    <div class="container-fluid">
-        <div class="row">
-            <div class="col-8 mx-auto">
-                <div style="height:100px; width:100%;background-color:#000;margin-top:3rem;"></div>
+        <nav class="nav" style="padding:25px 0px; background-color:#ffffff">
+            <li class="nav-item">
+                <h1 class="h1">
+                    <a class="" style="margin-right:20px;color:rgb(6, 105, 222);text-decoration:none;font-weight:700" href="<?= 'globo_com' ?>">Globo.com</a>    
+                </h1>
+            </li>
+
+            <li class="nav-item" style="margin-top:16px">
+                <a class="" style="margin-left:14px;font-size:20px;color:#a80000;text-decoration:none;font-weight:500;" href="<?= 'g1' ?>">G1</a>
+            </li>
+
+            <li class="nav-item" style="margin-top:16px">
+                 <a class="" style="margin-left:14px;font-size:20px;color:#3b9c00;text-decoration:none;font-weight:500;" href="<?= 'globo_esporte' ?>">Globo esporte</a>
+            </li>
+
+            <li class="nav-item" style="margin-top:16px">
+                <a class="" style="margin-left:14px;font-size:20px;color:#666;text-decoration:none;font-weight:500" href="<?= 'tech' ?>">tech</a>
+            </li>
+
+            <li class="nav-item" style="margin-top:19px">
+                <a class="hover-links" style="margin-left:14px;font-size:11px;color:#777;text-decoration:none;font-weight:500;text-transform:uppercase" href="<?= 'videos' ?>">assine já</a>
+            </li>
+            <li class="nav-item" style="margin-top:19px">
+                <a class="hover-links" style="margin-left:14px;font-size:11px;color:#777;text-decoration:none;font-weight:500;text-transform:uppercase" href="<?= 'videos' ?>">minha conta</a>
+            </li>
+            <li class="nav-item" style="margin-top:19px">
+                <a class="hover-links" style="margin-left:14px;font-size:11px;color:#777;text-decoration:none;font-weight:500;text-transform:uppercase" href="<?= 'videos' ?>">e-mail</a>
+            </li>
+
+            <form action="" class="form-inline">
+                <input type="text"  placeholder="encontre na globo.com" style="margin-left:20px; padding:0px" class="form-control">
+            </form>
+        </nav>
+        
+    <div class="row">
+        <div class="jumbotron" style="width:100%;padding:4rem 27rem;margin:20px 17px"></div>
+            
+            <div class="col" style="border-right:1px solid #ccc;">
+                <a href="" style="text-decoration:none"><h2 class="h2 text-danger">Bombardeio a bases inicia série de ataques aos EUA, afirma irã</h2></a>
+               <a class="text-danger" style="text-decoration:none;" href=""> <h6 class="h6">Reino Unido não sairá do acordo nuclear com o irã, diz porta-voz</h6></a>
+            
+            <div class="d-flex bd-highlight">
+                <div class="p-1 flex-fill bd-highlight">
+                    <div style="width:100px;height:100px;background-color:#000"></div>
+                    <a class="text-danger font-weight-bold" href=""><h5>Lorem Ipsum is simply</h5></a>
+                    <p class="paragraph text-danger" style="font-size:14px">Lorem Ipsum is simply</p>
+                </div>
+                <div class="p-1 flex-fill bd-highlight">
+                    <div style="width:100px;height:100px;background-color:#000"></div>
+                    <a class="text-danger font-weight-bold" href=""><h5>Lorem Ipsum is simply</h5></a>
+                    <p class="paragraph text-danger" style="font-size:14px">Lorem Ipsum is simply</p>
+                </div>
+                <div class="p-1 flex-fill bd-highlight">
+                    <div style="width:100px;height:100px;background-color:#000"></div>
+                    <a class="text-danger font-weight-bold" href=""><h5>Lorem Ipsum is simply</h5></a>
+                    <p class="paragraph text-danger" style="font-size:14px">Lorem Ipsum is simply</p>
+                </div>
+                <div  class="dropdown-divider"></div>
+               
             </div>
+            <div class="d-flex bd-highlight" style="margin-top:30px">
+                    <div class="p-1 flex-fill bd-highlight">
+                      <a class="text-danger" style="text-decoration:none" href=""><h4 class="h5 text-danger">Lorem Ipsum is simply</h4></a>
+                    </div>
+                    <div class="p-1 flex-fill bd-highlight">
+                      <a class="text-danger" style="text-decoration:none" href=""><h4 class="h5 text-danger">Lorem Ipsum is simply</h4></a>
+                    </div>
+                    <div class="p-1 flex-fill bd-highlight">
+                      <a class="text-danger" style="text-decoration:none" href=""><h4 class="h5 text-danger">Lorem Ipsum is simply</h4></a>
+                    </div>
+                </div>
+           
+            </div>
+           
+    <div class="col">
+        <div class="d-flex bd-highlight">
+                <div class="p-1 flex-fill bd-highlight">
+                    <div style="width:200px;height:100px; background-color:#000"></div>
+                    <a class="text-success text-left" style="text-decoration:none;" href=""><h6 style="color:#359e00">mattos deixa cruzeiro quatro dias após aceotar ajudar o clube</h6></a>
+                </div>  
+                <div class="p-1 flex-fill bd-highlight">
+                    <div style="width:200px;height:100px; background-color:#000"></div>
+                    <a class="text-left"  style="text-decoration:none;color:#ff8100;" href=""><h6 style="color:#ff8100">mattos deixa cruzeiro quatro dias após aceotar ajudar o clube</h6></a>
+                </div>     
+            </div>
+
+            <div class="d-flex bd-highlight">
+                <div class="p-1 flex-fill bd-highlight">
+                    <div style="width:200px;height:100px; background-color:#000"></div>
+                    <a class="text-success text-left" style="text-decoration:none;" href=""><h6 style="color:#359e00">mattos deixa cruzeiro quatro dias após aceotar ajudar o clube</h6></a>
+                    <p class="paragraph" style="font-size:14px;"><a class="text-success" style="color:#ff8100;font-size:14px;" href="">Atacante chega ao Goiás</a></p>
+                    <p class="paragraph" style="font-size:14px;"><a class="text-success" style="color:#ff8100;font-size:14px;" href="">Gurarin e Vasco longe de acordo</a></p>
+                </div>  
+                <div class="p-1 flex-fill bd-highlight">
+                    <div style="width:200px;height:100px; background-color:#000"></div>
+                    <a class="text-left"  style="text-decoration:none;color:#ff8100;" href=""><h6 style="color:#ff8100">mattos deixa cruzeiro quatro dias após aceotar ajudar o clube</h6></a>
+                    <p class="paragraph" style="font-size:14px;"><a style="color:#ff8100;" href="">Paloma enfrenta Diogo</a></p>
+                    <p class="paragraph" style="font-size:14px;"><a style="color:#ff8100;" href="">Mural de fotos choca Marcos</a></p>
+                </div>     
+            </div>
+            <div class="d-flex bd-highlight">
+                <div class="p-1 flex-fill bd-highlight">
+                <a style="text-decoration:none;" href=""><p class="paragraph" style="color:#359e00;font-size:1.2rem;">STJD rejeita punir Fiqueirense e manda caso à Procuradoria</p></a>
+                </div>
+                <div class="p-1 flex-fill bd-highlight">
+                    <a style="text-decoration:none;" href=""><p class="paragraph" style="color:#ff8100;font-size:1.2rem;margin-bottom:6px !important">Só amigos? Afonso se derrete para Lola</p></a>
+                    <a href="" style="text-decoration:none;"><p class="paragraph" style="color:#ff8100;font-size:1rem;">Éramos':Clotilde fica grávida</p></a>
+                </div>
         </div>
-   </div>
+        </div>
+        
+    </div>
+</div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
